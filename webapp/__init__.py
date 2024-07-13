d36c2bdbabb4c13f8f93f619170985049ba822a8
DavydovPA <magotov92@internet.ru>
2024-07-13 17:09:52 +0300

    Приведено в порядок графическая часть(начальная), со всем функционалом( главная-логин-регистрация)

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, flash, redirect, url_for
 from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
 from webapp.models import db, User
-from webapp.forms import LoginForm
+from webapp.forms import LoginForm, RegistrationForm
 from webapp.create_feed import feed_generator
 
 
@@ -23,7 +23,8 @@ def create_app():
 
     @app.route('/')
     def index():
-        return "Youtube to RSS"
+        title = "YouTubeToRSS"
+        return render_template('home.html', page_title=title)
 
     @app.route('/login')
     def login():
@@ -65,5 +66,26 @@ def create_app():
         filename = feed_generator()
         filepath = f'/rss/{filename}'
         return render_template('rss.html', rss_file=filepath)
+
+    @app.route('/register')
+    def register():
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        form = RegistrationForm()
+        title = "Регистрация"
+        return render_template('registration.html', page_title=title, form=form)
+
+    @app.route('/process-reg', methods=['POST'])
+    def process_reg():
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            new_user = User(username=form.username.data, email=form.email.data, role='user')
+            new_user.set_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Вы успешно зарегистрировались')
+            return redirect(url_for('login'))
+        flash('Исправьте ошибки в форме')
+        return redirect(url_for('register'))
 
     return app
