0ed417d1376b8e5103e28784519818b7ef454092
koolbasov <kolbasov.anton@gmail.com>
2024-11-01 13:49:13 +0300

    fixed flake8 errors

--- webapp/create_feed.py
+++ webapp/create_feed.py
@@ -8,19 +8,19 @@ from webapp import config
 
 def feed_generator(feed_id):
     fg = FeedGenerator()
-    fg.load_extension('podcast')
+    fg.load_extension("podcast")
 
     my_feed = Feed.query.filter(Feed.id == feed_id).first()
     fg.title(my_feed.feed_title)
     lang = Language.query.filter(Language.id == my_feed.lang_id).first()
     fg.language(lang.identifier)
-    fg.link(href=f'{config.LOCAL_SITE_URL}')
+    fg.link(href=f"{config.LOCAL_SITE_URL}")
     fg.description(my_feed.feed_description)
     fg.pubDate(my_feed.feed_pubDate.replace(tzinfo=pytz.UTC))
     fg.lastBuildDate(my_feed.lastBuildDate.replace(tzinfo=pytz.UTC))
     fg.podcast.itunes_image(f"{config.LOCAL_SITE_URL}/static/covers/{my_feed.feed_image}")
-    fg.podcast.itunes_author('Made by YoutubeToAudioPodcast')
-    fg.podcast.itunes_explicit('no')
+    fg.podcast.itunes_author("Made by YoutubeToAudioPodcast")
+    fg.podcast.itunes_explicit("no")
 
     my_feed_podcasts = Podcast.query.filter(Podcast.feed_id == my_feed.id).all()
     for podcast in my_feed_podcasts:
@@ -28,11 +28,15 @@ def feed_generator(feed_id):
         fe.title(podcast.podcast_title)
         fe.link(href=podcast.ytb_link)
         fe.description(podcast.ytb_description)
-        fe.enclosure(url=f"{config.LOCAL_SITE_URL}/static/podcasts/{podcast.enclosure}", length=podcast.duration, type="audio/mpeg")
+        fe.enclosure(
+            url=f"{config.LOCAL_SITE_URL}/static/podcasts/{podcast.enclosure}",
+            length=podcast.duration,
+            type="audio/mpeg",
+        )
         fe.guid(podcast.guid)
         fe.pubDate(podcast.pubDate.replace(tzinfo=pytz.UTC))
-        email, author = podcast.ytb_author.split('\n')
-        fe.author({'name': author, 'email': email})
+        email, author = podcast.ytb_author.split("\n")
+        fe.author({"name": author, "email": email})
         fe.podcast.itunes_duration(podcast.duration)
 
     fg.rss_str(pretty=True)
--- webapp/languages_for_db.py
+++ webapp/languages_for_db.py
@@ -1,22 +1,20 @@
 from webapp.models import db, Language
 
 languages = {
-    "English":  "en",
-    "French":  "fr",
-    "German":  "de",
-    "Italian":  "it",
-    "Russian":  "ru",
-    "Spanish":  "es",
+    "English": "en",
+    "French": "fr",
+    "German": "de",
+    "Italian": "it",
+    "Russian": "ru",
+    "Spanish": "es",
 }
 
 
 def add_languages_to_db(languages):
     for language_name, short_name in languages.items():
-        language_exists = Language.query.filter(
-            Language.identifier == short_name).count()
+        language_exists = Language.query.filter(Language.identifier == short_name).count()
         if not language_exists:
-            new_language = Language(
-                language=language_name, identifier=short_name)
+            new_language = Language(language=language_name, identifier=short_name)
             db.session.add(new_language)
             db.session.commit()
 
