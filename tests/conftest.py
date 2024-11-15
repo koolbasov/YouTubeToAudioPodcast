26fff5bf8b363e1e976aef2ecfb92e4598031c84
koolbasov <kolbasov.anton@gmail.com>
2024-11-15 12:59:07 +0300

    tests for get_xml_html.py is ready

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,18 @@ def valid_play_list():
 
 
 @pytest.fixture()
-def url_id_right_play_list():
+def private_play_list():
+    return "https://www.youtube.com/playlist?list=PLvO-3MXl8QMhfte7dEGGWp2NEA8PfAA-Q"
+
+
+@pytest.fixture()
+def url_id_valid_play_list():
     return "PLbr8rVGhPD0WQgO97Ao67Q-QVuSbm_Zpz"
+
+
+@pytest.fixture()
+def url_id_private_play_list():
+    return "PLvO-3MXl8QMhfte7dEGGWp2NEA8PfAA-Q"
 
 
 @pytest.fixture()
@@ -24,3 +34,13 @@ def integer():
 @pytest.fixture()
 def return_none():
     return None
+
+
+@pytest.fixture()
+def html_text():
+    return "<p>Hello Worlds!</p>"
+
+
+@pytest.fixture()
+def xml_text():
+    return '<feed xmlns:yt="http://www.youtube.com/xml/schemas/2015"></feed>'
--- tests/podcasts/test_get_xml_html.py
+++ tests/podcasts/test_get_xml_html.py
@@ -1,13 +1,19 @@
+import requests
 from unittest.mock import patch
 import pytest
 
-from webapp.podcast.parser.get_xml_html import get_url_id_from_youtube_link, is_valid_playlist
+from webapp.podcast.parser.get_xml_html import (
+    get_url_id_from_youtube_link,
+    is_valid_playlist,
+    get_html_from_youtube,
+    get_xml_from_youtube,
+)
 
 
 @pytest.mark.parametrize(
     "url, expected_result",
     [
-        ("valid_play_list", "url_id_right_play_list"),
+        ("valid_play_list", "url_id_valid_play_list"),
         ("not_a_play_list", "return_none"),
         ("integer", "return_none"),
     ],
@@ -16,12 +22,77 @@ def test__get_url_id_from_youtube__expected_behavior(url, expected_result, reque
     assert get_url_id_from_youtube_link(request.getfixturevalue(url)) == request.getfixturevalue(expected_result)
 
 
-def test__get_url_id_from_youtube__two_arguments(valid_play_list, not_a_play_list):
-    with pytest.raises(TypeError):
-        get_url_id_from_youtube_link(valid_play_list, not_a_play_list)
+def test__is_valid_playlist__valid_url(valid_play_list):
+    with patch("requests.get") as mock_request:
+        mock_request.return_value.status_code = 200
+        assert is_valid_playlist(valid_play_list) is True
 
 
-# def test__is_valid_playlist__valid_playlist(valid_play_list):
-#     with (patch("requests.get") as requests_get_mock,):
-#         requests_get_mock.status_code.return_value = 200
-#         assert is_valid_playlist(valid_play_list) is True
+def test__is_valid_playlist__not_valid_url(private_play_list):
+    with patch("requests.get") as mock_request:
+        mock_request.return_value.status_code = 400
+        assert is_valid_playlist(private_play_list) is False
+
+
+def test__is_valid_playlist__connection_error(valid_play_list):
+    with patch("requests.get", side_effect=requests.exceptions.ConnectionError) as mock_request:
+        mock_request.return_value.side_effect = requests.exceptions.ConnectionError
+        assert is_valid_playlist(valid_play_list) is False
+
+
+def test__get_html_from_youtube__valid_url(valid_play_list, html_text):
+    with patch("requests.get") as mock_request:
+        mock_request.return_value.status_code = 200
+        mock_request.return_value.text = html_text
+        assert get_html_from_youtube(valid_play_list) == html_text
+
+
+def test__get_html_from_youtube__not_valid_url(private_play_list):
+    with patch("requests.get") as mock_request:
+        mock_request.return_value.status_code = 400
+        assert get_html_from_youtube(private_play_list) is None
+
+
+def test__get_html_from_youtube__connection_error(valid_play_list):
+    with patch("requests.get", side_effect=requests.exceptions.ConnectionError) as mock_request:
+        mock_request.return_value.side_effect = requests.exceptions.ConnectionError
+        assert get_html_from_youtube(valid_play_list) is None
+
+
+def test__get_xml_from_youtube__valid_url(valid_play_list, url_id_valid_play_list, xml_text):
+    with (
+        patch("webapp.podcast.parser.get_xml_html.get_url_id_from_youtube_link") as mock_get_url_id_from_youtube_link,
+        patch("requests.get") as mock_request,
+    ):
+        mock_get_url_id_from_youtube_link.return_value = url_id_valid_play_list
+        mock_request.return_value.status_code = 200
+        mock_request.return_value.text = xml_text
+        assert get_xml_from_youtube(valid_play_list) == xml_text
+
+
+def test__get_xml_from_youtube__not_valid_url(private_play_list, url_id_private_play_list):
+    with (
+        patch("webapp.podcast.parser.get_xml_html.get_url_id_from_youtube_link") as mock_get_url_id_from_youtube_link,
+        patch("requests.get") as mock_request,
+    ):
+        mock_get_url_id_from_youtube_link.return_value = url_id_private_play_list
+        mock_request.return_value.status_code = 400
+        assert get_xml_from_youtube(private_play_list) is None
+
+
+def test__get_xml_from_youtube__not_a_playlist(not_a_play_list):
+    with (
+        patch("webapp.podcast.parser.get_xml_html.get_url_id_from_youtube_link") as mock_get_url_id_from_youtube_link,
+    ):
+        mock_get_url_id_from_youtube_link.return_value = None
+        assert get_xml_from_youtube(not_a_play_list) is None
+
+
+def test__get_xml_from_youtube__connection_error(valid_play_list, url_id_valid_play_list):
+    with (
+        patch("webapp.podcast.parser.get_xml_html.get_url_id_from_youtube_link") as mock_get_url_id_from_youtube_link,
+        patch("requests.get", side_effect=requests.exceptions.ConnectionError) as mock_request,
+    ):
+        mock_get_url_id_from_youtube_link.return_value = url_id_valid_play_list
+        mock_request.return_value.side_effect = requests.exceptions.ConnectionError
+        assert get_xml_from_youtube(valid_play_list) is None
